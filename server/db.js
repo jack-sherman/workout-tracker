addb43ed2fd21f9cc5d446a5d79bdf126251ba09
jack-sherman <jack.s9578@gmail.com>
2023-07-31 13:00:37 -0700

    test api and code refactoring a little bit

--- server/db.js
+++ server/db.js
@@ -1,12 +1,18 @@
 const pg = require("pg");
 require('dotenv').config();
+var pool;
 
-
-const pool = new pg.Pool({
+const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.INSTANCE_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
-});
-exports = {pool}
+};
+module.exports = {
+    getPool: function() {
+        if (pool) return pool;
+        pool = new pg.Pool(config);
+        return pool;
+    }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const pool = require('./db');
+const db = require('./db');
+const pool = db.getPool();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,9 +11,29 @@ app.use(cors());
 app.use(express.json());
 
 
+app.get('/api/users', async(req, res)=> {
+    try{
+        const users = await pool.query("SELECT * FROM users");
+        res.json(users.rows)
+    }
+    catch(err) {
+        console.error(err.message);
+    }
+});
 
+app.post('/api/signup', async(req, res) => {
+    const {username, password} = req.body;
+    try{
+        const addedUser = await pool.query(
+            "INSERT INTO users (username, password) VALUES ($1, $2)", [username, password]
+        );
+        console.log(addedUser)
+    } catch (err){
+        console.log(err);
+    }
+}
+)
 
 app.listen(port, () => {
     console.log(`server has started on port: ${port}`);
-    console.log(pool)
 });
